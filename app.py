529a1a3d6c5cba77bec01d88d3dd699c3292271d
dogunfx <dogunfx@gmail.com>
2023-03-09 12:56:22 +0100

    commited changes

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ from db import countries
 app = Flask(__name__)
 
 
-# git manages our code
-# github helps to host our online
-
 @app.route('/')
 def home():
-    return countries
+    return "Hello"
